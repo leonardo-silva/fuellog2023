6b7173deddf276a3d3280885bf0d883decedeffc
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-02-25 16:05:45 -0300

    Configuracao inicial do projeto - first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>FuelLog 2023!</Text>
+    </View>
+  );
+}
+
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: '#0B420C',
+    primary_light: '#12A454',
+    secondary: '#C8191E',
+    background: '#E5E5E5',
+    
+    header_text: '#FFFFFF',
+    text: '#000000',
+    text_light: '#969CB2',
+    
+    highlight: '##FF872C',
+    card_background: '#363F5F',
+
+  },
+  fonts: {
+
+  }
+}

949aa7931453f135816be7f9e98085fd8d0b1753
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-03-12 16:12:08 -0300

    Tela Home - Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,41 @@
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  CarInfo,
+  CarIcon,
+  HeaderTextWrapper,
+  Greeting,
+  CarPlate,
+  LogoutButton,
+  LogoutIcon 
+} from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Title>FuelLog 2023!</Title>
+      <Header>
+        <UserWrapper>
+          <CarInfo>
+            <CarIcon name='directions-car'>
+
+            </CarIcon>
+            <HeaderTextWrapper>
+              <Greeting>
+                Placa
+              </Greeting>
+              <CarPlate>
+                AAA-9999
+              </CarPlate>
+            </HeaderTextWrapper>
+          </CarInfo>
+          <LogoutButton>
+            <LogoutIcon name='logout'>
+
+            </LogoutIcon>
+          </LogoutButton>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,63 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Title = styled.Text`
-  font-size: 23px;
-  font-family: ${({ theme }) => theme.fonts.medium};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(40)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  //align-items: center;
+  //justify-content: center;
 `;
+
+export const UserWrapper = styled.View`
+  flex-direction: row;
+  padding: 0 24px;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+`;
+
+export const CarInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CarIcon = styled(MaterialIcons)`
+  color: ${({ theme }) => theme.colors.header_text};
+  font-size: ${RFValue(55)}px;
+`;
+
+export const HeaderTextWrapper = styled.View`
+  margin-left: ${RFValue(26)}px;
+`;
+
+export const Greeting = styled.Text`
+  color: ${({ theme }) => theme.colors.header_text};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  line-height: ${RFValue(24)}px;
+`;
+
+export const CarPlate = styled.Text`
+  color: ${({ theme }) => theme.colors.header_text};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
+  line-height: ${RFValue(24)}px;
+`;
+
+export const LogoutButton = styled.View``;
+
+export const LogoutIcon = styled(MaterialIcons)`
+  color: ${({ theme }) => theme.colors.header_text};
+  font-size: ${RFValue(24)}px;
+
+`;
+
